18456483264765834e6665e74f641c4d3495aefa
boolenboom <alan93072@gmail.com>
2022-01-22 13:43:52 +0800

    component - navigationVue

---
+++ src/components/Navigation.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+<template>
+    <nav class="nav">
+        <a to="/">LOGO</a>
+        <div class="d-flex menu">
+            <a href="/#summary">Summary</a>
+            <a href="/#portfolio">Portfolio & Links</a>
+            <a href="/#skills">Skills</a>
+            <a href="/#experience">Experience</a>
+        </div>
+    </nav>
+</template>
