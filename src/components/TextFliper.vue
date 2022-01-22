1e9a1a5b1652359663d8910954e6597c9c51c112
boolenboom <alan93072@gmail.com>
2022-01-22 22:31:43 +0800

    更換動畫操作

--- src/components/TextFliper.vue
+++ src/components/TextFliper.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onMounted, reactive, ref, toRefs, watch } from 'vue';
-import eventChannel from '../assets/public-js/eventChannel.js';
+
 const props = defineProps({
     textList: {
         type: Array,
@@ -20,25 +20,15 @@ let stateList = reactive([]);
 
 watch( textList, ()=>{
     listFresh();
-    listListen();
 });
 onMounted( ()=>{
     listFresh();
-    listListen();
 } );
 
 function listFresh(){
     stateList.length = textList.value.length;
-    stateList.fill( 'running', 0 );
-}
-function listListen(){
-    stateList.forEach( (val, currId)=>{
-        eventChannel
-        .channel( `state${ currId }` )
-        .listen( 'run',  function(){
-            stateList[currId] = 'running';
-        });
-    })
+    stateList[0] = 'running';
+    stateList.fill( 'paused', 1 );
 }
 
 let duration = ref(2);
@@ -46,12 +36,12 @@ let duration = ref(2);
 function animationCoolDown(ev){
     const currId = ev.target.dataset.id;
     const length = textList.value.length;
+    const nextId = (+currId + 1) % length
     stateList[currId] = 'paused';
-    console.log( currId + ' is paused at ' + Date() );
-    eventChannel
-    .channel( `state${ (+currId + 1) % length }` )
-    .trigger( 'run' );
-    //issue - 背景作業時，時間間距會變長，導致動畫順序亂掉
+    stateList[nextId] = 'running';
+}
+function isRun(id){
+    return (stateList[id] == 'running')
 }
 </script>
 <template>
@@ -59,10 +49,9 @@ function animationCoolDown(ev){
         <h1 v-for=" (text, index) of textList" 
             class="text"
             :data-id=" index "
-            :style="`--duration: ${ (duration / 0.8) }s;
-                    --delay: ${ index * ((duration / 0.8) * (1 - 0.1)) }s;
-                    --playState: ${ stateList[index] }`"
-            @animationiteration="animationCoolDown"> 
+            :class="{ 'active' : isRun(index) }"
+            :style="`--duration: ${ (duration / 0.8) }s;`"
+            @animationend="animationCoolDown"> 
             {{text}}
         </h1>
         <h1 class="hidden">textHeight</h1>
@@ -79,16 +68,17 @@ function animationCoolDown(ev){
         position: absolute;
         top: 0;
         width: fit-content;
-        animation: flipUp 
-            var( --duration, 0s )  
-            var( --delay, 0s ) 
-            infinite 
-            var( --playState, 'paused') ;
         transform: translateY( 100% );
     }
     .hidden{
         opacity: 0;
         pointer-events: none;
+    }
+    .active{
+        animation: flipUp 
+            var( --duration, 0s )  
+            var( --delay, 0s ) 
+            1;
     }
     @keyframes flipUp {
         0%{
