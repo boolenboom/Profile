a458c1fe2b551b57849fc6a883d7052783954d53
boolenboom <alan93072@gmail.com>
2022-01-24 00:50:03 +0800

    新增 NavigationVue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <script setup>
 import HelloWorld from '../components/HelloWorld.vue';
 import TextFliper from '../components/TextFliper.vue';
+import Navigation from '../components/Navigation.vue';
 
 </script>
 <template>
-<HelloWorld></HelloWorld>
+<Navigation></Navigation>
+<HelloWorld msg="Part 1"></HelloWorld>
 <TextFliper></TextFliper>
+<HelloWorld msg="Part 2"></HelloWorld>
+<HelloWorld msg="Part 3"></HelloWorld>
+<HelloWorld msg="Part 4"></HelloWorld>
+<HelloWorld msg="Part 5"></HelloWorld>
 </template>
