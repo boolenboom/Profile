f417109a31c85167b789a2b57be91b9aef70d869
boolenboom <alan93072@gmail.com>
2022-01-23 23:42:44 +0800

    設定 vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue';
+import TextFliper from '../components/TextFliper.vue';
+
+</script>
+<template>
+<HelloWorld></HelloWorld>
+<TextFliper></TextFliper>
+</template>
