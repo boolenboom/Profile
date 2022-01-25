2fd68be7ecd348c58384e707ca2c2f5eaf594a00
boolenboom <alan93072@gmail.com>
2022-01-25 23:11:00 +0800

    新增 summary 並套入 homeVue

---
+++ src/components/Summary.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+<template>
+<div class="enterAnimation"></div>
+<section id="summary" class="fullScreen">
+    <div class="container">
+        <h1>Summary</h1>
+        <p>
+            喜歡透過各種方式來尋找能達成目標且一石二鳥的解決辦法，文章、實作經驗、談話中找出有價值的資訊，幫助我用更多角度去挖掘問題本質。
+            在大學裡累積了很多數位內容的實作經驗，培養了對開發的興趣，透過Vue框架幫家人的工作坊做出有RWD的主視覺介紹網頁；
+            串接交通部的公共運輸整合資訊流通服務平臺(PTX)做出旅遊網站，以及弄一些簡單的小嘗試增進技術的應用。
+        </p>
+        <p>
+            在快速變化的前端領域裡，需要的是敏捷思維以及持續學習的精神，這對我來說就是會源源不絕噴出寶物的地方，等著我去挖掘。
+            也希望能夠更深入軟體領域，認識更多新事物，並且創造價值。
+        </p>
+    </div>
+</section>
+
+</template>
+<style lang="scss">
+#summary{
+    .container{
+        text-align: start;
+        h1{
+            font-size: 2rem;
+        }
+        p{
+            margin: 1.5rem 0;
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,13 @@
 import HelloWorld from '../components/HelloWorld.vue';
 import Navigation from '../components/Navigation.vue';
 import Hero from '../components/Hero.vue';
+import Summary from '../components/Summary.vue';
 
 </script>
 <template>
 <Navigation></Navigation>
 <Hero></Hero>
+<Summary></Summary>
 <HelloWorld msg="Part 1"></HelloWorld>
 <HelloWorld msg="Part 2"></HelloWorld>
 <HelloWorld msg="Part 3"></HelloWorld>
