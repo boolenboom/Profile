757ce91f63948f2122eba475def503a03ca9c4fe
boolenboom <alan93072@gmail.com>
2022-03-11 22:00:34 +0800

    調整Hero 內容、佈局

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,20 +1,12 @@
 <script setup>
-import { ref } from 'vue';
 import TextFliper from './TextFliper.vue';
 
-let maskGraph = ref("M224 50Q250 28 270 50S320 80 400 290Q 467 447 384 440S10 420 0 360Q0 330 14 300S60 200 224 50Z");
 </script>
 <template>
-<section id="hero">
-    <div class="container dis-flex">
-        <div class="graph">
-            <div class="mask" :style="`--mask-graph: '${maskGraph}';`">
-                <div class="bg-blue img"></div>
-            </div>
-        </div>
+<section id="hero" class="dis-flex center">
         <div class="intro">
             <h1>嗨 !</h1>
-            <div class="dis-flex greet"><h1> 我是 </h1><TextFliper :textList="['李東遠', 'Front-End Developer.', '前端工程師']"></TextFliper></div>
+            <div class="dis-flex"><h1> 我 </h1><TextFliper :textList="['是李東遠', '追尋核心', '是前端工程師']"></TextFliper></div>
             <div class="personalInfo">
                 <h3>臺中教育大學 數位內容科技學系</h3>
                 <h3>0911572605</h3>
@@ -25,7 +17,6 @@ let maskGraph = ref("M224 50Q250 28 270 50S320 80 400 290Q 467 447 384 440S10 42
                 <a href=""><button>CV</button></a>
             </div>
         </div>
-    </div>
 </section>
 </template>
 <style lang="scss">
@@ -33,32 +24,35 @@ let maskGraph = ref("M224 50Q250 28 270 50S320 80 400 290Q 467 447 384 440S10 42
     height: 100vh;
     pointer-events: none;
     h1{
-        font-size: 3rem;
-        @include pad-width{
-            font-size: 2em;
+        font-size: 2em;
+        @include pc-width{
+            font-size: 8rem;
         }
         margin: 0;
     }
-    .container{
-        justify-content:space-evenly;
-        align-items: center;
-        flex: 0 0 content;
-        flex-wrap: wrap;
-    }
     .intro{
+        width: 80%;
         text-align: start;
         z-index: 1;
         .personalInfo{
-            margin: 30px 6px;
+            text-align: end;
+            margin: 120px 6px;
+            @include pc-width{
+                margin: 36px 6px;
+            }
             h3{
-                margin: 6px 0;
+                font-size: 1rem;
+                margin: .2rem 0;
+                @include pc-width{
+                    font-size: 2rem;
+                    margin: .5rem 0;
+                }
             }
         }
-        .greet{
-            gap: 1rem;
-        }
         .otherDocument{
+            text-align: end;
             button{
+                pointer-events: fill;
                 padding: 0.8em 1.5em;
                 margin-right: .6em;
                 background-color: $main-color-secondary;
@@ -66,20 +60,6 @@ let maskGraph = ref("M224 50Q250 28 270 50S320 80 400 290Q 467 447 384 440S10 42
                 border-radius: .8em;
                 color: #fff;
             }
-        }
-    }
-    .graph{
-        @include pad-width{
-            transform: scale(.6);
-        }
-        .mask{
-            width: 500px;
-            height: 500px;
-            clip-path: path(var(--mask-graph));
-        }
-        .img{
-            width: 99%;
-            height: 99%;
         }
     }
 }
