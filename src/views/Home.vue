da23a25ca01fa0dd52d788082b4d4cb212a6c7f1
boolenboom <alan93072@gmail.com>
2022-06-12 01:10:33 +0800

    將轉場動畫放進TransitionAnimation組件內

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,24 @@
 <script setup>
-import HelloWorld from '../components/HelloWorld.vue';
 import Navigation from '../components/Navigation.vue';
 import Hero from '../components/Hero.vue';
 import Summary from '../components/Summary.vue';
 import Portfolio from '../components/Portfolio.vue';
 import FixedBackground from '../components/FixedBackground.vue';
+import TransitionAnimation from '../components/TransitionAnimation.vue';
+import TextFlashMarquee from '../components/TextFlashMarquee.vue';
+import BlackHole from '../components/BlackHole.vue';
 
 </script>
 <template>
 <Navigation></Navigation>
 <FixedBackground></FixedBackground>
 <Hero></Hero>
+<TransitionAnimation class="hero2summary">
+    <TextFlashMarquee flashText="summary"></TextFlashMarquee>
+</TransitionAnimation>
 <Summary></Summary>
+<TransitionAnimation class="summary2portfolio">
+    <BlackHole aniText="work&links"></BlackHole>
+</TransitionAnimation>
 <Portfolio></Portfolio>
-<HelloWorld msg="Part 1"></HelloWorld>
-<HelloWorld msg="Part 2"></HelloWorld>
-<HelloWorld msg="Part 3"></HelloWorld>
-<HelloWorld msg="Part 4"></HelloWorld>
-<HelloWorld msg="Part 5"></HelloWorld>
 </template>
