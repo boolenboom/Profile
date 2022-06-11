92ca3eb2352ba32e85021bd5ccd660822a93c8cb
boolenboom <alan93072@gmail.com>
2022-06-12 01:00:06 +0800

    新增組件 - 轉場動畫

---
+++ src/components/TransitionAnimation.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="transitionAnimation fullScreen">
+    <div class="pin">
+        <slot></slot>
+    </div>
+</div>
+</template>
+<style>
+.transitionAnimation{
+    position: relative;
+    pointer-events: none;
+    z-index: 2;
+}
+</style>
