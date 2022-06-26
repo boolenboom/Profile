100a27845096c28d35415d82b9faa4f637678921
boolenboom <alan93072@gmail.com>
2022-06-27 00:46:01 +0800

    新增組件 - rotate text effect button並套用於portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,5 @@
 <script setup>
-import BlackHole from './BlackHole.vue';
+import RotateTextBtn from './RotateTextBtn.vue';
 import RadialSlider from './RadialSlider.vue';
 import ScrollDetect from './ScrollDetect.vue';
 import { onMounted, ref } from 'vue';
@@ -20,38 +20,63 @@ onMounted(()=>{
     <div class="portfolio">
         <div id="works">
             <ScrollDetect :startPoint='start'>
-                    <RadialSlider 
-                    :imageList="['https://picsum.photos/id/69/1400/800',
+                <RadialSlider :imageList="['https://picsum.photos/id/69/1400/800',
                                 'https://picsum.photos/id/666/1400/800',
                                 'https://picsum.photos/id/678/1400/800',
-                                'https://picsum.photos/id/123/1400/800']"
-                    :contentAmount="4">
-                        <template #slide1>
-                            <h2>Travel Taiwan</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                            <a href="#0" class="btn">前往</a>
-                        </template>
-                        <template #slide2>
-                            <h2>CSS Rush</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                            <a href="#0" class="btn">前往</a>
-                        </template>
-                        <template #slide3>
-                            <h2>Lee's profile</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                            <a href="#0" class="btn">前往</a>
-                        </template>
-                        <template #slide4>
-                            <h2>Slide #4 Title</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
-                            <a href="#0" class="btn">前往</a>
-                        </template>
-                    </RadialSlider>
+                                'https://picsum.photos/id/123/1400/800']" :contentAmount="4">
+                    <template #title1>Travel Taiwan</template>
+                    <template #slide1>
+                        <article>
+                            <div class="workImg"></div>
+                            <h2 class="text-large">Travel Taiwan</h2>
+                            <p class="text-small">使用公共運輸整合資訊流通服務平台(Public Transport data eXchange
+                                ,PTX)的資料，從串接到處理資料的過程寫成小套件提高程式碼複用性，且可以在不同專案使用。
+                                能使用搜尋的方式尋找到想要的資訊。</p>
+                            <RotateTextBtn url="#123" BtnText="go to Travel Taiwan..." class="toBottom">
+                            </RotateTextBtn>
+                        </article>
+                    </template>
+                    <template #title2>CSS Rush</template>
+                    <template #slide2>
+                        <article>
+                            <div class="workImg"></div>
+                            <h2 class="text-large">CSS Rush</h2>
+                            <p class="text-small">pet-landing - 切版到完成耗時14小時，使用CSS偽類、Input tag、label tag
+                                製作簡易輪播功能，並包含RWD。</p>
+                            <RotateTextBtn url="#456" BtnText="go to CSS Rush..." class="toBottom">
+                            </RotateTextBtn>
+                        </article>
+                    </template>
+                    <template #title3>Lee's profile</template>
+                    <template #slide3>
+                        <article>
+                            <div class="workImg"></div>
+                            <h2 class="text-large">Lee's profile</h2>
+                            <p class="text-small">使用GSAP、THREEJS創建豐富特效的個人網站，自行設計網站的布局和動畫，
+                                把個人特質和對世界的想法融入網站樣貌。</p>
+                            <RotateTextBtn url="#789" BtnText="go to Lee's profile..." class="toBottom">
+                            </RotateTextBtn>
+                        </article>
+                    </template>
+                    <template #title4>Github Repos</template>
+                    <template #slide4>
+                        <article>
+                            <div class="workImg"></div>
+                            <h2 class="text-large">Github Repos</h2>
+                            <p class="text-small">參考GitHub RESTful API 官方函式庫(Octokit)的使用方式，在自行編寫的套件上擴充方法，
+                                讓取得資料的過程簡化。可以快速地看到GitHub的使用者清單，
+                                以及使用者的Public Repos，並透過Router快速搜尋到特定資訊。</p>
+                            <RotateTextBtn url="#010" BtnText="go to Github Repos..." class="toBottom">
+                            </RotateTextBtn>
+                        </article>
+                    </template>
+                </RadialSlider>
             </ScrollDetect>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
+
 .portfolio{
     position: relative;
 }
@@ -62,6 +87,44 @@ onMounted(()=>{
     z-index: 1;
     > *{
         background-color: $main-color-secondary;
+    }
+}
+article{
+    position: relative;
+    display: grid;
+    grid-template:  "head head" auto
+                    "img context" 1fr
+                    "img context" 84px / 30vw 1fr;
+    gap: 36px;
+    height: 60vh;
+    @include phone-width{
+        grid-template: "head" auto
+                        "img" 3fr
+                        "context" 1fr / auto;
+        gap: 12px;
+        height: 80vh;
+    }
+    h2{
+        text-align: start;
+        grid-area: head / head / head / head;
+    }
+    p{
+        text-align: start;
+        z-index: 2;
+        grid-area: context / context / context / context;
+    }
+    .workImg{
+        position: absolute;
+        top: 5vh;
+        left: 5vw;
+        width: 100%;
+        height: 600px;
+        grid-area: img;
+        background-color: $main-color;
+    }
+    .toBottom{
+        grid-area: img;
+        position: absolute;
     }
 }
 .initial{
---
+++ src/components/RotateTextBtn.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+const props = defineProps({
+    url:{
+        type:String,
+        default:'#'
+    },
+    BtnText:{
+        type:String,
+        default:'Btn................'
+    }
+})
+</script>
+<template>
+    <a :href="url" class="rotate-Btn">
+        <svg viewBox="0 0 84 84">
+            <defs>
+                <path id="pathCircle" stroke="#fff" fill="none" d="M42 0a42 42 0 0 1 0 84a42 42 0 0 1 0 -84z">
+                </path>
+            </defs>
+            <text>
+                <textPath href="#pathCircle">{{BtnText}}</textPath>
+            </text>
+        </svg>
+    </a>
+</template>
+<style lang="scss">
+
+.rotate-Btn {
+    --fz: 24px;
+    --btn-radius: 84px;
+    @include phone-width{
+        --fz: 24px;
+        --btn-radius: 42px;
+    }
+    font-size: var(--fz);
+    display: block;
+    inline-size: var(--btn-radius);
+    block-size: var(--btn-radius);
+    line-height: var(--btn-radius);
+    will-change: transform;
+    animation: touchRotate 8s linear infinite paused forwards;
+    @include phone-width{
+        animation-play-state: running;
+    }
+
+    svg{
+        overflow: unset;
+    }
+
+    textPath {
+        letter-spacing: 2px;
+    }
+
+    &:hover {
+        animation-play-state: running;
+    }
+}
+@keyframes touchRotate {
+    from {
+        transform: rotate(1turn);
+    }
+}
+</style>
