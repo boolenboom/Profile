a5167b3cf186aff0b84f801f18252c9b7fb6448d
boolenboom <alan93072@gmail.com>
2022-06-27 19:35:06 +0800

    調整summary布局

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -20,16 +20,18 @@ onMounted(()=>{
 })
 </script>
 <template>
-    
+
     <section id="summary" class="fullScreen" :style="{'--progress':scroll.progress}">
         <div class="container">
-            <h1>About me</h1>
-            <p>
+            <h1 class="text-large">About me</h1>
+            <p class="text-medium text-align-start">
                 喜歡透過各種方式來尋找能達成目標且一石二鳥的解決辦法，文章、實作經驗、談話中找出有價值的資訊，幫助我用更多角度去挖掘問題本質。
+            </p>
+            <p class="text-medium ">
                 在大學裡累積了很多數位內容的實作經驗，培養了對開發的興趣，透過Vue框架幫家人的工作坊做出有RWD的主視覺介紹網頁；
                 串接交通部的公共運輸整合資訊流通服務平臺(PTX)做出旅遊網站，以及弄一些簡單的小嘗試增進技術的應用。
             </p>
-            <p>
+            <p class="text-medium text-align-end">
                 在快速變化的前端領域裡，需要的是敏捷思維以及持續學習的精神，這對我來說就是會源源不絕噴出寶物的地方，等著我去挖掘。
                 也希望能夠更深入軟體領域，認識更多新事物，並且創造價值。
             </p>
@@ -45,7 +47,17 @@ onMounted(()=>{
         text-align: start;
         color: $text-color;
         p {
-            margin: 0.5rem 0;
+            margin: 2rem 0;
+            &.text-align-start{
+                margin-inline-end: 52.5%;
+            }
+            &.text-align-end{
+                margin-inline-start: 52.5%;
+                text-align: start;
+            }
+        }
+        p ~ p{
+            margin-block-start: 8rem;
         }
     }
 }
