ba484644f9dfa901e7bca3da0e1c618cd835bca4
boolenboom <alan93072@gmail.com>
2022-07-29 20:55:00 +0800

    更換圖片傳入組建的方式，確保build對圖片進行hash & 換上Profile 3D mockup背景圖

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -3,8 +3,10 @@ import RotateTextBtn from './RotateTextBtn.vue';
 import RadialSlider from './RadialSlider.vue';
 import ScrollDetect from './ScrollDetect.vue';
 import { onMounted, ref } from 'vue';
-
-
+import img_travel_taiwan from '../assets/portfolio/travel-taiwan_3DMockUp.png';
+import img_pet_landing from '../assets/portfolio/pet-landing_3DMockUp.png';
+import img_profile from '../assets/portfolio/profile_3DMockUp.png';
+import img_github_repos from '../assets/portfolio/github-repos_3DMockUp.png';
 
 let start = ref(0);
 function initial(){
@@ -16,14 +18,17 @@ onMounted(()=>{
     window.addEventListener('resize', initial);
     window.addEventListener('load', initial);
 })
+let imageList = ref([
+    img_travel_taiwan,
+    img_pet_landing,
+    img_profile,
+    img_github_repos
+    ])
 </script>
 <template>
     <div id="portfolio">
         <ScrollDetect :startPoint='start'>
-            <RadialSlider :imageList="['src/assets/portfolio/travel-taiwan_3DMockUp.png',
-            'src/assets/portfolio/pet-landing_3DMockUp.png',
-            'src/assets/portfolio/github-repos_3DMockUp.png',
-            'src/assets/portfolio/github-repos_3DMockUp.png']" :contentAmount="4">
+            <RadialSlider :imageList="imageList" :contentAmount="4">
                 <template #title1>Travel Taiwan</template>
                 <template #slide1>
                     <article>
@@ -31,25 +36,35 @@ onMounted(()=>{
                             <!-- <img src="../assets/portfolio/travel-taiwan.png" alt=""> -->
                         </div>
                         <h2 class="text-large">Travel Taiwan</h2>
-                        <p class="text-small">使用公共運輸整合資訊流通服務平台(Public Transport data eXchange
-                            ,PTX)的資料，從串接到處理資料的過程寫成小套件提高程式碼複用性，且可以在不同專案使用。
-                            能使用搜尋的方式尋找到想要的資訊。</p>
-                        <a href="#123" class="jump-url">
-                            <RotateTextBtn>go to Travel Taiwan...</RotateTextBtn>
+                        <p class="text-small">
+                            ．Vue2、Vue Router、AJAX、SCSS．<br />
+                            使用公共運輸整合資訊流通服務平台(Public Transport data eXchange
+                            , PTX)的資料，從串接到處理資料的過程寫成小套件提高程式碼複用性，以及確保欄位無資料時的處理。<br />
+                            使用第三屆的前端修練精神時光屋(THE F2E)設計稿(<a
+                                href="https://www.figma.com/file/IBlZc32IdQ46jUVtfOHxZn/w1%E5%8F%B0%E7%81%A3%E6%97%85%E9%81%8A%E6%99%AF%E9%BB%9E%E5%B0%8E%E8%A6%BD?node-id=67%3A220"
+                                target="_blank" rel="noopener noreferrer">figma連結</a>)並依照設計稿完成樣式和變版。
+                        </p>
+                        <!-- case 6656 click event trigger -->
+                        <a href="https://travel-taiwan.netlify.app/" target="_blank" rel="noopener noreferrer"
+                            class="jump-url">
+                            <RotateTextBtn>go to travel taiwan···</RotateTextBtn>
                         </a>
                     </article>
                 </template>
-                <template #title2>CSS Rush</template>
+                <template #title2>Pet-landing</template>
                 <template #slide2>
                     <article>
                         <div class="workImg">
                             <!-- <img src="../assets/portfolio/pet-landing.png" alt=""> -->
                         </div>
-                        <h2 class="text-large">CSS Rush</h2>
-                        <p class="text-small">pet-landing - 切版到完成耗時14小時，使用CSS偽類、Input tag、label tag
-                            製作簡易輪播功能，並包含RWD。</p>
-                        <a href="#456" class="jump-url">
-                            <RotateTextBtn>go to CSS Rush...</RotateTextBtn>
+                        <h2 class="text-large">Pet-landing</h2>
+                        <p class="text-small">
+                            ．HTML、CSS． <br />
+                            完全參照monster template的網頁Pet-landing重製作，切版到完成耗時14小時，使用CSS偽類、Input tag、label
+                            tag製作簡易輪播功能，並包含RWD。</p>
+                        <a href="https://boolenboom.github.io/CSS-Rush/pet-landing/" target="_blank"
+                            rel="noopener noreferrer" class="jump-url">
+                            <RotateTextBtn>go to pet-landing···</RotateTextBtn>
                         </a>
                     </article>
                 </template>
@@ -60,10 +75,14 @@ onMounted(()=>{
                             <!-- <img src="../assets/portfolio/github-repos.png" alt=""> -->
                         </div>
                         <h2 class="text-large">Lee's profile</h2>
-                        <p class="text-small">使用GSAP、THREEJS創建豐富特效的個人網站，自行設計網站的布局和動畫，
-                            把個人特質和對世界的想法融入網站樣貌。</p>
-                        <a href="#789" class="jump-url">
-                            <RotateTextBtn>go to Lee's profile...</RotateTextBtn>
+                        <p class="text-small">
+                            ．Vue3、Vite、GSAP、SCSS．<br />
+                            使用GSAP創建豐富特效的個人網站，自行設計網站的布局和動畫，
+                            把個人特質和對世界的想法融入網站樣貌。
+                        </p>
+                        <a href="https://boolenboom.github.io/Profile/" target="_blank" rel="noopener noreferrer"
+                            class="jump-url">
+                            <RotateTextBtn>go to lee's profile···</RotateTextBtn>
                         </a>
                     </article>
                 </template>
@@ -74,11 +93,13 @@ onMounted(()=>{
                             <!-- <img src="../assets/portfolio/github-repos.png" alt=""> -->
                         </div>
                         <h2 class="text-large">Github Repos</h2>
-                        <p class="text-small">參考GitHub RESTful API 官方函式庫(Octokit)的使用方式，在自行編寫的套件上擴充方法，
-                            讓取得資料的過程簡化。可以快速地看到GitHub的使用者清單，
-                            以及使用者的Public Repos，並透過Router快速搜尋到特定資訊。</p>
-                        <a href="#012" class="jump-url">
-                            <RotateTextBtn>go to Github Repos...</RotateTextBtn>
+                        <p class="text-small">
+                            ．React、React Router、Vite、AJAX．<br />
+                            參考GitHub RESTful API 官方函式庫(Octokit)的使用方式，在自行編寫的套件上擴充方法，
+                            讓取得資料的過程簡化。可以快速地看到GitHub的使用者清單、Repos，並透過Router快速搜尋到特定資訊。</p>
+                        <a href="https://react-repos-list.netlify.app/" target="_blank" rel="noopener noreferrer"
+                            class="jump-url">
+                            <RotateTextBtn>go to github repos···</RotateTextBtn>
                         </a>
                     </article>
                 </template>
@@ -114,15 +135,24 @@ article{
     }
     h2{
         color: $main-color-secondary;
-        text-shadow: rgba($color: #fff, $alpha: .8) 2px 3px 0;
+        text-shadow: 
+        rgba($color: #fff, $alpha: .8) 2px 2px 0,
+        rgba($color: #fff, $alpha: .8) -2px 2px 0,
+        rgba($color: #fff, $alpha: .8) 2px -2px 0,
+        rgba($color: #fff, $alpha: .8) -2px -2px 0;
         text-align: start;
         grid-area: head / head / head / head;
     }
     p{
         font-weight: 700;
         text-align: start;
-        background-color: #ffffffdd;
+        background-color: #ffffffaa;
         color: $main-color-secondary;
+        a{
+            color: $main-color-secondary;
+            text-decoration:underline;
+        }
+        backdrop-filter: blur(2px);
         padding: 12px;
         border-radius: 12px;
         height: fit-content;
@@ -144,6 +174,10 @@ article{
         grid-area: img;
         position: absolute;
         top: 12px;
+        padding: 24px;
+        background-color: rgba($color: #fff, $alpha: .73);
+        border-radius: 50%;
+        backdrop-filter: blur(2px);
         &::after{
             content: '';
             position: absolute;
@@ -154,8 +188,11 @@ article{
             margin: auto;
             width: 36px;
             height: 36px;
-            background-color: #000;
+            background-color: $main-color;
             clip-path: polygon(0 84%, 16% 100%, 80% 35%, 80% 78%, 100% 60%, 100% 0, 40% 0, 21% 20%, 65% 20%);
+        }
+        &:hover > *{
+            animation-play-state: running;
         }
     }
 }
