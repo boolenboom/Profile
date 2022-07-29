4dbd0684e37f608f85081ae2f383904e7050b353
boolenboom <alan93072@gmail.com>
2022-07-29 20:49:28 +0800

    確保plugin載入

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'path';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import router from 'vue-router';
 const pathSrc = path.resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),router],
   resolve:{
       alias:{
       '/@': pathSrc,
