de1fcc000f7ee3710b84759ea090c02ec8892674
boolenboom <alan93072@gmail.com>
2022-07-29 21:38:45 +0800

    修正build index.html url

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const pathSrc = path.resolve(__dirname, 'src');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),router],
+  base:'/Profile/',
   resolve:{
       alias:{
       '/@': pathSrc,
